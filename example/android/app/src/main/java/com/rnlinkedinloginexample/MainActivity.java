6f8ac5d5995b887e9bdc48d3e2ab5e42a12fb231
Jody Brewster <jodybrewster@gmail.com>
2016-02-18 13:31:28 -0500

    bump to rn 0.18.0

--- example/android/app/src/main/java/com/rnlinkedinloginexample/MainActivity.java
+++ example/android/app/src/main/java/com/rnlinkedinloginexample/MainActivity.java
@@ -1,8 +1,11 @@
 package com.rnlinkedinloginexample;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.KeyEvent;
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.Arrays;
+import java.util.List;
 
 import net.jodybrewster.linkedinlogin.RNLinkedinLoginModule;        // <------ add here
 import net.jodybrewster.linkedinlogin.RNLinkedinLoginPackage;       // <------ add here
@@ -10,84 +13,44 @@ import net.jodybrewster.linkedinlogin.RNLinkedinLoginPackage;       // <------ a
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.linkedin.platform.LISessionManager;
 
+public class MainActivity extends ReactActivity {
 
-import com.facebook.react.LifecycleState;
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.ReactRootView;
-import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
-
-public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
-
-    private ReactInstanceManager mReactInstanceManager;
-    private ReactRootView mReactRootView;
-
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mReactRootView = new ReactRootView(this);
-
-        mReactInstanceManager = ReactInstanceManager.builder()
-                .setApplication(getApplication())
-                .setBundleAssetName("index.android.bundle")
-                .setJSMainModuleName("index.android")
-                .addPackage(new MainReactPackage())
-                .addPackage(new VectorIconsPackage())
-                .addPackage(new RNLinkedinLoginPackage(this))
-                .setUseDeveloperSupport(BuildConfig.DEBUG)
-                .setInitialLifecycleState(LifecycleState.RESUMED)
-                .build();
-
-        mReactRootView.startReactApplication(mReactInstanceManager, "RNLinkedinLoginExample", null);
-
-        setContentView(mReactRootView);
+    protected String getMainComponentName() {
+        return "RNLinkedinLoginExample";
     }
 
-    // add this method inside your activity class
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, android.content.Intent data) {
+    /**
+     * Returns whether dev mode should be enabled.
+     * This enables e.g. the dev menu.
+     */
+    @Override
+    protected boolean getUseDeveloperSupport() {
+        return BuildConfig.DEBUG;
+    }
+
+   /**
+   * A list of packages used by the app. If the app uses additional views
+   * or modules besides the default ones, add more packages here.
+   */
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+        new MainReactPackage(),
+        new VectorIconsPackage(),
+        new RNLinkedinLoginPackage(this));
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, android.content.Intent data) {
       LISessionManager.getInstance(getApplicationContext()).onActivityResult(this, requestCode, resultCode, data); // <------ add here
-  }
 
-    @Override
-    public boolean onKeyUp(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
-            mReactInstanceManager.showDevOptionsDialog();
-            return true;
-        }
-        return super.onKeyUp(keyCode, event);
+      super.onActivityResult(requestCode, resultCode, data);
     }
 
-    @Override
-    public void onBackPressed() {
-      if (mReactInstanceManager != null) {
-        mReactInstanceManager.onBackPressed();
-      } else {
-        super.onBackPressed();
-      }
-    }
-
-    @Override
-    public void invokeDefaultOnBackPressed() {
-      super.onBackPressed();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onPause();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onResume(this, this);
-        }
-    }
+  
 }
